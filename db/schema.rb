3fa5b80d538c69327c98933447a3aab81ef3eb5f
Scott Ballantyne <ussballantyne@gmail.com>
2012-08-13 16:14:28 +0800

    adding roles

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120813074951) do
+ActiveRecord::Schema.define(:version => 20120813081251) do
 
   create_table "answers", :force => true do |t|
     t.integer  "choice_id"
@@ -126,6 +126,17 @@ ActiveRecord::Schema.define(:version => 20120813074951) do
     t.string   "question_type"
   end
 
+  create_table "roles", :force => true do |t|
+    t.string   "name"
+    t.integer  "resource_id"
+    t.string   "resource_type"
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
+  end
+
+  add_index "roles", ["name", "resource_type", "resource_id"], :name => "index_roles_on_name_and_resource_type_and_resource_id"
+  add_index "roles", ["name"], :name => "index_roles_on_name"
+
   create_table "scorecards", :force => true do |t|
     t.integer  "project_id"
     t.string   "sector"
@@ -192,5 +203,12 @@ ActiveRecord::Schema.define(:version => 20120813074951) do
     t.datetime "updated_at",      :null => false
     t.string   "name"
   end
+
+  create_table "users_roles", :id => false, :force => true do |t|
+    t.integer "user_id"
+    t.integer "role_id"
+  end
+
+  add_index "users_roles", ["user_id", "role_id"], :name => "index_users_roles_on_user_id_and_role_id"
 
 end
