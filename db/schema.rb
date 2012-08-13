d8209b64330495ae7337cc7e20681e9111a374c6
Scott Ballantyne <ussballantyne@gmail.com>
2012-08-13 15:50:35 +0800

    adding name to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120813051454) do
+ActiveRecord::Schema.define(:version => 20120813074951) do
 
   create_table "answers", :force => true do |t|
     t.integer  "choice_id"
@@ -190,6 +190,7 @@ ActiveRecord::Schema.define(:version => 20120813051454) do
     t.string   "password_digest"
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
+    t.string   "name"
   end
 
 end
