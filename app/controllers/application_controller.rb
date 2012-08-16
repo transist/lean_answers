9cb6223e6cea74de8a6ccb9d8af89cef5b961add
simsicon <simsicon@gmail.com>
2012-08-16 10:26:27 +0800

    Using api of devise, and added notice and alert.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   private  
-    
-  def logged_in?  
-    current_user != false
-  end
   
   def current_users_projects
     (current_user ? current_user.projects : [])
   end
   
   helper_method :current_users_projects
-  helper_method :logged_in?
+  helper_method :user_signed_in?
   include Mobylette::RespondToMobileRequests
 end
