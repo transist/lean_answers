48bb754d463d75dc72c0f707ca1e7d3cab53b9a8
Rainux Luo <rainux@gmail.com>
2012-08-24 16:06:28 +0800

    Avoid use helper for css class

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,31 +2,8 @@ module ApplicationHelper
   def new_hypothesis_title(hypothesis)
     "New #{hypothesis.class.to_s.gsub('Hypothesis', '')} Hypothesis"
   end
-  
+
   def edit_hypothesis_title(hypothesis)
     "Editing #{hypothesis.class.to_s.gsub('Hypothesis', '')} Hypothesis"
-  end
-  
-  def state_css(state)
-    case state
-    when 'current'
-      'label-info'
-    when 'pending'
-      ''
-    when 'rejected'
-      'label-inverse'
-    when 'accepted'
-      'label-success'
-    end
-  end
-  def type_css(type)
-    case type
-    when 'SolutionHypothesis'
-      'label-important'
-    when 'ProblemHypothesis'
-      'label-warning'
-    when 'CustomerHypothesis'
-      'label-info'
-    end
   end
 end
